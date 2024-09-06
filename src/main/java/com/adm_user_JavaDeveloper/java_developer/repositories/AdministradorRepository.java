fd3000a1a3f670ed132a01e3c793819e8ed48827
GustavoLima67 <lgusta333@gmail.com>
2024-09-06 17:30:16 -0300

    Modificando

--- src/main/java/com/adm_user_JavaDeveloper/java_developer/repositories/AdministradorRepository.java
+++ src/main/java/com/adm_user_JavaDeveloper/java_developer/repositories/AdministradorRepository.java
@@ -5,5 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.adm_user_JavaDeveloper.java_developer.entities.Administrador;
 
 public interface AdministradorRepository extends JpaRepository<Administrador, Long>{
-
 }
