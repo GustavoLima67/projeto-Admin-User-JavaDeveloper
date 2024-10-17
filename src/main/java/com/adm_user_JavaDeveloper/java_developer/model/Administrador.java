3a70db34494728155cdb3bfbb63cbb2c8c5651fa
GustavoLima67 <lgusta333@gmail.com>
2024-10-16 22:34:32 -0300

    Projeto finalizado - Dando inicio a nova implementação - REACT JS

--- src/main/java/com/adm_user_JavaDeveloper/java_developer/model/Administrador.java
+++ src/main/java/com/adm_user_JavaDeveloper/java_developer/model/Administrador.java
@@ -8,18 +8,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity(name = "administrador")
 @Table(name  = "administrador")
 public class Administrador implements Serializable {
 
-	@ManyToOne
-	@JoinColumn(name = "usuarios_id")
-	Usuarios user;
-	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
@@ -74,14 +68,6 @@ public class Administrador implements Serializable {
 	
 	public void setSenha(String senha) {
 		this.senha = senha;
-	}
-
-	public Usuarios getUser() {
-		return user;
-	}
-	
-	public void setUser(Usuarios user) {
-		this.user = user;
 	}
 
 	public LocalDate getDataNascimento() {
