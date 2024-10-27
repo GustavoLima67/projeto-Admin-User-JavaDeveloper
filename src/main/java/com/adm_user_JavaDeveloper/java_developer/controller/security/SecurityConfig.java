3b97cc05ed8778a6f3dc5073c1a3df6730cd3bfb
GustavoLima67 <lgusta333@gmail.com>
2024-10-27 19:54:23 -0300

    Eliminando 1 linha de codigo - Reativando a proteção CSRF

--- src/main/java/com/adm_user_JavaDeveloper/java_developer/controller/security/SecurityConfig.java
+++ src/main/java/com/adm_user_JavaDeveloper/java_developer/controller/security/SecurityConfig.java
@@ -12,7 +12,6 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-            .csrf().disable() // Desativa a proteção CSRF (temporariamente)
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/api/usuarios").permitAll() // Permite acesso sem autenticação
                 .anyRequest().authenticated()
